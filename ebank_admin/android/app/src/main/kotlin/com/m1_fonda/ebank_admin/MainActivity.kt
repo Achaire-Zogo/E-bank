53b9eaa340f9c42e03fbee3bd7166c09d7301758
Achaire_Zogo <61749305+Achaire-Zogo@users.noreply.github.com>
2024-11-08 16:37:56 +0100

    first commit

---
+++ ebank_admin/android/app/src/main/kotlin/com/m1_fonda/ebank_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.m1_fonda.ebank_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ebank_user/android/app/src/main/kotlin/com/ebank_user/ebank_user/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ebank_user.ebank_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
